1468cbd5e5d10d7d2503f600821113c33c955037
Marwan465 <marwanshade@gmail.com>
2024-10-17 20:15:12 +0300

    Obelion Task

--- Terraform-aws-module/modules/mysql_rds/main.tf
+++ Terraform-aws-module/modules/mysql_rds/main.tf
@@ -1,4 +1,15 @@
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "my-single-subnet-group"
+  subnet_ids = [var.subnet_id]  
+
+  tags = {
+    Name = "My Single Subnet Group"
+  }
+}
+
 resource "aws_db_instance" "mysql" {
+ 
   allocated_storage      = var.allocated_storage
   engine                 = "mysql"
   engine_version         = var.engine_version
@@ -7,6 +18,7 @@ resource "aws_db_instance" "mysql" {
   username               = var.username
   password               = var.password
   publicly_accessible    = var.publicly_accessible
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [var.security_group_id]
 
   skip_final_snapshot = true
